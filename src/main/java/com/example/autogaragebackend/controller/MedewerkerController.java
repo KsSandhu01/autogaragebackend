535a290ee8d8ca7b6c7f49f46dc730b3d00fcae4
Kawal <kawalpreet.singhsandhu@novi-education.nl>
2023-02-05 22:05:22 +0100

    Clean code. Ongebruikte code en imports verwijderd

--- src/main/java/com/example/autogaragebackend/controller/MedewerkerController.java
+++ src/main/java/com/example/autogaragebackend/controller/MedewerkerController.java
@@ -5,6 +5,7 @@ import com.example.autogaragebackend.model.Medewerker;
 import com.example.autogaragebackend.service.MedewerkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/v1/medewerkers")
+@PreAuthorize("hasAuthority('ROLE_ADMINISTRATIEFMEDEWERKER')")
 public class MedewerkerController {
 
     @Autowired
